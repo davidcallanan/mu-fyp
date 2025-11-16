cbdd472bf6dbc5edc6b08a67707acdef2e137f72
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2025-11-16 10:30:07 +0000

    add compiler cli

--- compiler/README.md
+++ compiler/README.md
@@ -8,3 +8,7 @@ The compilation process is separated into two phases:
 - `backend` (C++): Remaining analysis and transformations + codegen using LLVM.
 
 While I intended to write the `backend` in JavaScript, the primary interface for LLVM is `C++`-based, so I'm writing this phase in `C++` for simplicity.
+
+## Setup
+
+- Install `pnpm ^9.15.1`.
---
+++ compiler/ec.js
@@ -0,0 +1,30 @@
+import { Command } from "commander";
+import { process as frontend_process } from "./frontend/process.js";
+
+const program = new Command();
+
+const backend_process = () => { };
+
+const compile_module = async (config) => {
+	console.log("Compiling module located at:", config.src);
+
+	const result = await frontend_process(config);
+	await backend_process(result);
+};
+
+program
+	.name("ec")
+	.description("Essence C compiler")
+	.version("1.0.0");
+
+program
+	.command("compile <src> <dest>")
+	.description("Compile a module located at <src> and output build artifacts to <dest>")
+	.action(async (src, dest, _options) => {
+		await compile_module({
+			src,
+			dest,
+		});
+	});
+
+program.parse();
