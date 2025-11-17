a88936da06fc25eac346f741e1a6591db4d5dd3e
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2025-11-17 10:10:28 +0000

    fix backend build process

--- compiler/backend/host/run.sh
+++ compiler/backend/host/run.sh
@@ -1,2 +1,2 @@
-docker build -t davidcallanan--mu-fyp--compiler-backend .
+docker build -t davidcallanan--mu-fyp--compiler-backend . && \
 docker run -it -v "$(pwd)/in:/volume/in" -v "$(pwd)/out:/volume/out" davidcallanan--mu-fyp--compiler-backend
